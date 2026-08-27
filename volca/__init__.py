4f707fca5538ee5f0f244a2e43d2bdf12e71da2b
janintibo -art <janintibo@gmail.com>
2026-08-27 06:35:41 +0200

    v2.1.1: MANAGE_EXTERNAL_STORAGE redeclare, retour API 33

--- volca/__init__.py
+++ volca/__init__.py
@@ -1,6 +1,6 @@
 """volca-gain : preparation et transfert de samples pour Korg volca sample."""
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 APP_NAME = "MOC'TA BASS"
 
 from . import (audio, batch, bibliotheque, etat, kit, librarian,
